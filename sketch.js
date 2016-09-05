18fd4f51ff6d929613dd5eaaea3be8ccfd2efdee
Eric <ericc3141@users.noreply.github.com>
2016-09-05 01:16:17 -0400

    Convert sketch.js to strict mode

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var grid = function(){
 	this.id=grid.ID;
 	grid.ID ++;
@@ -6,13 +8,17 @@ var grid = function(){
 	document.body.style.height = bodyHeight + "px";
 	this.prevX;
 	this.prevY;
+	this.ctx;
+	this.maxX=-1;this.maxY=-1;
 };
 grid.ID=0;
 var bodyWidth = window.innerWidth;
 var bodyHeight = window.innerHeight;
 
 grid.prototype.createCanvas = function(x, y){
-	canvas = document.createElement("canvas");
+	if (x > this.maxX){this.maxX = x;}
+	if (y > this.maxY){this.maxY = y;}
+	var canvas = document.createElement("canvas");
 	canvas.style.position = "absolute";
 	canvas.style.left = x* 255 + "px";
 	canvas.style.top = y * 255 + "px";
@@ -20,35 +26,36 @@ grid.prototype.createCanvas = function(x, y){
 	canvas.width = 255;
 	canvas.height = 255;
 	document.body.appendChild(canvas);
-	ctx = canvas.getContext("2d");
+	var ctx = canvas.getContext("2d");
 	ctx.fillStyle = "rgba(0,0,0,1)";
 	grid[x + "," + y] = ctx;
+	this.ctx=ctx;
 }
 
 grid.prototype.getCtx = function(x, y){
-	canvasX=Math.trunc(x/255);
-	canvasY=Math.trunc(y/255);
+	var canvasX=Math.trunc(x/255);
+	var canvasY=Math.trunc(y/255);
 	if(grid[canvasX + "," + canvasY] == null){
 		this.createCanvas(canvasX, canvasY);
 	}
-	return grid[canvasX + "," + canvasY];
+	this.ctx = grid[canvasX + "," + canvasY];
 }
 
 grid.prototype.draw = function(x, y){
 	//Calculate the tile locations for previous and current cursor locations
 	//Currently only works going downwards, to the right
-	pX=Math.trunc(this.prevX/255);
-	pY=Math.trunc(this.prevY/255);
-	cX=Math.trunc(x/255);
-	cY=Math.trunc(y/255);
+	var pX=Math.trunc(this.prevX/255);
+	var pY=Math.trunc(this.prevY/255);
+	var cX=Math.trunc(x/255);
+	var cY=Math.trunc(y/255);
 	//Loop through all tiles in rectangle. 
 	for(var i = Math.min(pX,cX);i <= Math.max(pX,cX);i ++){
 		for (var j = Math.min(pY,cY);j <= Math.max(pY,cY);j ++){
-			ctx = this.getCtx(i*255, j*255);
+			this.getCtx(i*255, j*255);
 			//compute coordinates relative to top left of tile
-			ctx.moveTo(this.prevX - i * 255, this.prevY - j * 255);
-			ctx.lineTo(x - i * 255, y - j * 255);
-			ctx.stroke();
+			this.ctx.moveTo(this.prevX - i * 255, this.prevY - j * 255);
+			this.ctx.lineTo(x - i * 255, y - j * 255);
+			this.ctx.stroke();
 		}
 	}
 	this.prevX = x;
@@ -56,10 +63,10 @@ grid.prototype.draw = function(x, y){
 }
 
 grid.prototype.move = function(x, y){
-	ctx = this.getCtx(x, y);
+	this.getCtx(x, y);
 	this.prevX = x;
 	this.prevY = y;
 	x %= 255;
 	y %= 255;
-	ctx.moveTo(x, y);
+	this.ctx.moveTo(x, y);
 }
